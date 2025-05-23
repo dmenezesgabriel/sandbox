b1490092f500e8c300b3293d90acab26f2524ed6
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-05-23 13:40:49 -0300

    feat: add storybook task app

---
+++ storybook-task-app/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-essentials",
+    // "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
---
+++ storybook-task-app/README.md
@@ -0,0 +1,17 @@
+- **Create a project**:
+
+```sh
+pnpm create vite@latest
+```
+
+- **Add storybook**:
+
+```sh
+pnpm create storybook@latest
+```
+
+- **Add storybook addons**:
+
+```sh
+pnpm install -D @storybook/addon-interactions @storybook/addon-links msw msw-storybook-addon
+```
