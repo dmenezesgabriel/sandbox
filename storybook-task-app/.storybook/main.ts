0b4ad6ee4115bac7aef4e95e6e042f76dc33bddd
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-05-23 23:01:25 -0300

    feat: storybook task component

--- storybook-task-app/.storybook/main.ts
+++ storybook-task-app/.storybook/main.ts
@@ -2,18 +2,31 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  core: {
+    disableTelemetry: true,
+  },
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-essentials",
     // "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
+    // "@storybook/experimental-addon-test",
     "@storybook/addon-interactions",
     "@storybook/addon-links",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+  viteFinal: (config) => {
+    config.optimizeDeps = {
+      ...(config.optimizeDeps || {}),
+      exclude: ["@storybook/addon-docs"],
+    };
+
+    return config;
+  },
 };
+
 export default config;
