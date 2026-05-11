e929c58cd6e097fe418f6265d0904d7c90adfe16
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2026-05-11 19:48:11 -0300

    fix: styles

--- portable_bi_ask/stylelint.config.js
+++ portable_bi_ask/stylelint.config.js
@@ -1,5 +1,13 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard'],
-  rules: {},
+  rules: {
+    'custom-property-pattern': null,
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+    'no-descending-specificity': null,
+    'declaration-block-no-duplicate-properties': true,
+    'comment-empty-line-before': null,
+    'comment-whitespace-inside': null,
+  },
 };
